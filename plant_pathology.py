8813a357258c0ab5806974742bfad335594f4723
avdosev <avdosev1@yandex.ru>
2020-05-15 20:28:05 +0400

    Исправил сохранение модели, убрал мусорные комментарии

--- plant_pathology.py
+++ plant_pathology.py
@@ -1,42 +1,20 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[33]:
-
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd 
 import os
 import cv2
 from tensorflow import keras
 import tensorflow as tf
 
-
-# In[2]:
-
-
-# mainPath = '../input/plant-pathology-2020-fgvc7'
 main_path = './dataset'
 images_path = os.path.join(main_path, 'images')
 test_path = os.path.join(main_path, 'test.csv')
 train_path = os.path.join(main_path, 'train.csv')
 
-
-# In[3]:
-
-
 test_data = pd.read_csv(test_path)
 train_data = pd.read_csv(train_path)
 
-
-# In[4]:
-
-
 train_data.head()
-
-
-# In[10]:
-
 
 def load_image(image_id):
     file_path = image_id + ".jpg"
@@ -47,47 +25,19 @@ def load_image(image_id):
     image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-
-# In[30]:
-
-
 input_shape = load_image(test_data['image_id'][0]).shape
 print(input_shape)
 
-
-# In[53]:
-
-
 X_train = train_data["image_id"].apply(lambda id: os.path.join(images_path, id + '.jpg'))
-
-
-# In[54]:
-
 
 X_train.head()
 
-
-# In[43]:
-
-
 y_train = train_data.loc[:, 'healthy':'scab']
-
-
-# In[44]:
-
 
 y_train.head()
 
-
-# In[67]:
-
-
 batch_size = 20
 epochs = 20
-
-
-# In[78]:
-
 
 def load_dataset(filename, res):
     bits = tf.io.read_file(filename)
@@ -97,21 +47,12 @@ def load_dataset(filename, res):
     return image, res
 
 
-# In[85]:
-
-
 train_dataset = (
     tf.data.Dataset
     .from_tensor_slices((X_train, y_train))
     .map(load_dataset)
-    .repeat()
-    .shuffle(256)
     .batch(batch_size)
 )
-
-
-# In[90]:
-
 
 model = keras.Sequential([
     keras.layers.Conv2D(64, (5, 5), input_shape=input_shape, activation='relu'),
@@ -135,34 +76,15 @@ model = keras.Sequential([
 ])
 
 
-# In[91]:
-
-
 model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
 
-# In[92]:
-
-
 model.summary()
-
-
-# In[93]:
 
 
 model.fit(train_dataset, epochs=20, steps_per_epoch=X_train.shape[0]//batch_size)
 
-
-# In[ ]:
-
-
+os.makedirs(os.path.join('.', 'models'))
 model.save(os.path.join('.', 'models', 'model.hdf5'))
-
-
-# In[ ]:
-
-
-
-
