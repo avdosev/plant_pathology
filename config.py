1e1a9f92d9a23f2d87773662382419122c0072e8
avdosev <avdosev1@yandex.ru>
2020-05-18 14:56:35 +0400

    Изменил количество эпох

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ train_path = os.path.join(main_path, 'train.csv')
 sub_path = os.path.join(main_path, 'sample_submission.csv')
 
 batch_size = 20
-epochs = 2
+epochs = 20
 input_shape = (273, 409, 3)
