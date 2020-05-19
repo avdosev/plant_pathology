0de8ac47d4f49ef59860263bb313e84b1f42c22d
avdosev <avdosev1@yandex.ru>
2020-05-19 17:29:42 +0400

    Добавил эпох

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ sub_path = os.path.join(main_path, 'sample_submission.csv')
 models_path = os.path.join('.', 'models')
 
 batch_size = 20
-epochs = 40
+epochs = 60
 # input_shape = (273, 409, 3)
 input_shape = (224, 224, 3)
